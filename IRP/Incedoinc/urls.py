55892a4afb8c16d5d30b8b44f2a680c8f55fb4d9
pulkit <pulkit171112236@gmail.com>
2021-01-26 18:55:51 +0530

    added create_candidate page

--- IRP/Incedoinc/urls.py
+++ IRP/Incedoinc/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
+    path("candidate/create", views.create_candidate_view, name="create_candidate_page")
 ]
--- IRP/Incedoinc/views.py
+++ IRP/Incedoinc/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Welcome to Incedo Portal')
+
+def create_candidate_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form_fields = request.POST
+        print(form_fields)
+        
+    context = {}
+    return render(request, 'create_candidate.html', context)
