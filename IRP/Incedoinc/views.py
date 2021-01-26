8cb54a5bba83408ba225677a68fee1988a26b74f
rudra pratap singh <rudra333pratap333@gmail.com>
2021-01-26 23:06:37 +0530

    Database models created

--- IRP/Incedoinc/views.py
+++ IRP/Incedoinc/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Employee, Job, Candidate, Feedback, CandidateJobInfo
+
 # Create your views here.
 def index(request):
     return HttpResponse('Welcome to Incedo Portal')
