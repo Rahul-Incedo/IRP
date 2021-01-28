0b31916d4d610a58f41bb091d089354ebc3bf0ad
pulkit <pulkit171112236@gmail.com>
2021-01-28 12:01:05 +0530

    update add_candidate
    There were 2 instances of add candidate pages. Kept add_candidate and removed create candidate.

--- IRP/Incedoinc/urls.py
+++ IRP/Incedoinc/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("candidate/create", views.create_candidate_view, name="create_candidate_page"),
     path("candidate/add", views.add_candidate_view, name="add_candidate_page"),
     
 ]
--- IRP/Incedoinc/views.py
+++ IRP/Incedoinc/views.py
@@ -11,12 +11,6 @@ from .forms import Candidate_details_form
 def index(request):
     return HttpResponse('Welcome to Incedo Portal')
 
-def create_candidate_view(request, *args, **kwargs):
-    if request.method == 'POST':
-        form_fields = request.POST
-        print(form_fields)
-    context = {}
-    return render(request, 'create_candidate.html', context)
 
 def add_candidate_view(request, *args, **kwargs):
     candidate_details_form = Candidate_details_form()
