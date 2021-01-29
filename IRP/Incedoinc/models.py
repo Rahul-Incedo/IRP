e5f15cdfdb7092a04d46ac946ec925162a71451e
rudra pratap singh <rudra333pratap333@gmail.com>
2021-01-29 21:01:53 +0530

    modified model

--- IRP/Incedoinc/models.py
+++ IRP/Incedoinc/models.py
@@ -2,81 +2,72 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    name = models.CharField(max_length=64)
-    emailId = models.EmailField(max_length=254, null=True)
-    incedoCode = models.CharField(max_length=64, primary_key=True, default=None)
+    full_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=254, null=True)
+    employee_id = models.CharField(max_length=64, primary_key=True, default=None)
     password = models.CharField(max_length=64)
-    tempPassword = models.CharField(max_length=64)
+    temp_password = models.CharField(max_length=64, null=True, blank=True)
 
     def __str__(self):
-        return f'{name}'
+        return f'{self.full_name}'
 
 
 class Job(models.Model):
-    requisitionId = models.CharField(max_length=100, primary_key=True)
-    raisedByEmployee = models.ForeignKey(Employee, null = True, related_name='raisedByEmployee', on_delete=models.CASCADE)
-    positionOwner = models.ForeignKey(Employee, null = True, related_name='positionOwner', on_delete=models.CASCADE)
-    #positionOwner = models.ManyToOneField(Employee, blank=True, related_name='owner')
-
-    description = models.FileField()
-    timestamp = models.DateTimeField()
+    requisition_id = models.CharField(max_length=64, primary_key=True)
+    raised_by_employee = models.ForeignKey(Employee, null = True, related_name='raisedByEmployee', on_delete=models.CASCADE)
+    position_owner_id = models.ForeignKey(Employee, null = True, related_name='positionOwner', on_delete=models.CASCADE)
+    job_description = models.FileField()
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{description}'
+        return f'{self.requisition_id}'
+
 
 class Candidate(models.Model):
-    firstName = models.CharField(max_length=64 )
-    middleName = models.CharField(max_length=64)
-    lastName = models.CharField(max_length=64 )
-    requisitionId = models.ForeignKey(Job, null=True, on_delete = models.CASCADE)
-    registeredBy = models.ForeignKey(Employee, null =True, on_delete = models.CASCADE )
-    emailId = models.EmailField(max_length=254, primary_key=True)
+    f_name = models.CharField(max_length=64)
+    m_name = models.CharField(max_length=64, null=True, blank=True)
+    l_name = models.CharField(max_length=64)
+    registered_by = models.ForeignKey(Employee, null =True, on_delete = models.CASCADE )
+    email = models.EmailField(max_length=254, primary_key=True)
     gender_choice = [('M', 'Male'),
                     ('F', 'Female')]
     gender = models.CharField(max_length=1,
                             choices= gender_choice)
-    universityRollNo = models.CharField(max_length=64 )
-    graduationCGPA = models.FloatField(null=True)
-    graduationYear = models.IntegerField(null=True)
-    collegeName = models.CharField(max_length = 254 )
+
+    CGPA = models.FloatField(null=True)
+    college_name = models.CharField(max_length = 254 )
     experience = models.IntegerField(null=True)
-    mobileNo = models.CharField(max_length=10)
+    mobile = models.CharField(max_length=10)
     DOB = models.DateField(auto_now = True)
-    projectsLink = models.URLField(null=True)
-    resume = models.FileField(upload_to=None, max_length=100)#path for saving the resume
+    projects_link = models.URLField(null=True)
+    resume = models.FileField(upload_to=None)
     noticePeriod = models.IntegerField(null=True)
     timestamp = models.DateTimeField()
 
+    @property
+    def full_name(self):
+    	return f'{self.f_name} {self.m_name} {self.l_name}'
+
     def __str__(self):
-        return f'{firstName} {lastName} : {emailId}'
+        return f'{self.f_name} : {self.email}'
 
 
 class Feedback(models.Model):
-    feedbackId = models.AutoField(primary_key=True)
-    candidateEmail = models.ForeignKey(Candidate, on_delete=models.CASCADE)
-    interviewerCode = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    feedback_id = models.AutoField(primary_key=True)
+    candidate_email = models.ForeignKey(Candidate, blank=True, null=True, on_delete=models.CASCADE)
+    interviewer_id = models.ForeignKey(Employee, blank=True, null=True, on_delete=models.CASCADE)
+    requisition_id = models.ForeignKey(Job, blank=True, on_delete = models.CASCADE)
     level = models.IntegerField(null=True)
-    status_choices = [('P', 'pass'),
-                        ('F', 'fail')]
-    status = models.CharField(max_length=1, choices = status_choices)
-    ratingPython = models.IntegerField(null=True)
-    ratingJava = models.IntegerField(null=True)
-    ratingCPP = models.IntegerField(null=True)
-    ratingSQL = models.IntegerField(null=True)
-    comments = models.TextField(max_length=500)
-    timestamp = models.DateTimeField()
+    status_choices = [('pass', 'pass'),
+                        ('Fail', 'fail'),
+                        ('Pending', 'pending'),]
+    status = models.CharField(choices = status_choices, max_length=10)
+    rating_python = models.IntegerField(null=True, blank=True)
+    rating_java = models.IntegerField(null=True, blank=True)
+    rating_cpp = models.IntegerField(null=True, blank=True)
+    rating_sql = models.IntegerField(null=True, blank=True)
+    comments = models.TextField(max_length=500, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Result for {candidateEmail} at level {level} is {status}'
-
-
-class CandidateJobInfo(models.Model):
-    jobInfoId =  models.AutoField(primary_key=True)
-    emailId = models.ForeignKey(Candidate, on_delete=models.CASCADE)
-    requisitionId = models.ForeignKey(Job, on_delete = models.CASCADE)
-    level1 = models.ForeignKey(Feedback, related_name='level1', null=True, on_delete = models.CASCADE)
-    level2 = models.ForeignKey(Feedback, related_name='level2', null=True, on_delete = models.CASCADE)
-    level3 = models.ForeignKey(Feedback, related_name='level3', null=True, on_delete = models.CASCADE)
-
-    def __str__(self):
-        return f'{jobInfoId}'
+        return f'{self.status}'
