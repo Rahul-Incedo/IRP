76481e042f77e30f3bb17d64f7ad17ccd1a91152
Vaishnavi Srivastava <72790971+VaishnaviSrivastavaCodes@users.noreply.github.com>
2021-02-01 00:07:12 +0530

    Refreshed
    hello

--- IRP/Incedoinc/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Employee, Job, Candidate, Feedback
-
-# Register your models here.
-admin.site.register(Employee)
-admin.site.register(Job)
-admin.site.register(Candidate)
-admin.site.register(Feedback)
