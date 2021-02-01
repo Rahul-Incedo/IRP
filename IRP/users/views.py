988c9114e7b605745a379b3a6c577ddd024ad856
rudra pratap singh <rudra333pratap333@gmail.com>
2021-02-01 08:57:34 +0530

    IRP
    Project done

--- IRP/users/views.py
+++ IRP/users/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from .forms import SignUpForm
 
+from django.contrib.auth.models import User
+
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
@@ -30,7 +32,11 @@ def signup_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
+            user_ = User.objects.create_user(username=str(form.cleaned_data['email']), password=form.cleaned_data['password'])
+            print(user_.username)
+            user_.save()
             user = form.save()
+
             return HttpResponseRedirect(reverse('home_page'))
     else:
         form = SignUpForm()
