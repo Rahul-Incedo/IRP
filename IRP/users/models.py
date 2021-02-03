17bcd1148af4566b774016e94bc0559c34a775d0
Vaishnavi Srivastava <72790971+VaishnaviSrivastavaCodes@users.noreply.github.com>
2021-02-03 16:02:01 +0530

    Added managers.py

--- IRP/users/models.py
+++ IRP/users/models.py
@@ -1,26 +1,27 @@
 
+from .managers import CustomUserManager
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser, PermissionsMixin, UserManager
 
 from django.contrib.auth import get_user_model
-User = get_user_model()
+#User = get_user_model()
 
 # https://www.youtube.com/watch?v=KO8n02g-Ezc
  
 class CustomUser(AbstractUser):
 
-    
-    username = models.EmailField(('Username'), max_length=150, unique=True)
+    employee_id = models.CharField(('Employee ID'), max_length= 50, unique = True)
+    username = models.EmailField(('Email'), max_length=150, unique=True)
     name = models.CharField(('Name'), max_length=150, blank=True)
-    email = models.EmailField(('Email Address'), blank=False, unique = True)
+    #email = models.EmailField(('Email Address'), blank=False, unique = True)
 
 
-    objects = UserManager()
+    objects = CustomUserManager()
 
    # EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['name', 'email',]
+    REQUIRED_FIELDS = ['name','email','employee_id']
 
 
     def __str__(self):
