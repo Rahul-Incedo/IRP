8dd951835b149cdc9269fddba7c8f4526eba7576
Pulkit Bajaj <pulkit171112236@gmail.com>
2021-03-08 11:22:39 +0530

    local changes

--- IRP/Incedoinc/templatetags/customfilters.py
+++ IRP/Incedoinc/templatetags/customfilters.py
@@ -3,15 +3,15 @@ from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
-@register.filter
-def cut(value, arg):
-    """Removes all values of arg from the given string"""
-    return value.replace(arg, '')
+# @register.filter
+# def cut(value, arg):
+#     """Removes all values of arg from the given string"""
+#     return value.replace(arg, '')
 
-@register.filter
-@stringfilter
-def custom_lower(value):
-    return value.lower()
+# @register.filter
+# @stringfilter
+# def custom_lower(value):
+#     return value.lower()
 
 @register.filter('get_value_from_dict')
 def get_value_from_dict(dict_data, key):
@@ -20,7 +20,3 @@ def get_value_from_dict(dict_data, key):
     """
     if key:
         return dict_data.get(key)
-
-# @register.filter
-# @stringfilter
-# def get
